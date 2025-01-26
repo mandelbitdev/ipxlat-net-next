721f3cf64bb0de487f98b32b0c5e634a2021db7c
Antonio Quartulli <antonio@mandelbit.com>
2025-01-26 03:01:17 +0100

    net: introduce the SIIT virtual device driver
    This virtual device driver allows creating interfaces
    impelmenting the basic IPv6 SIIT logic for translating
    IPv6 packets into IPv4.
    
    Along with the NAT44 and NAT66 mechanisms, this module
    allows to implement full NAT64 in-kernel.
    
    ** TO BE EXTENDED **
    
    Signed-off-by: Alberto Leiva Popper <ydahhrk@gmail.com>
    Signed-off-by: Antonio Quartulli <antonio@mandelbit.com>

---
+++ drivers/net/joolif/icmp.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-2.0
+/*  SIIT - Stateless IP/ICMP Translation virtual device driver
+ *
+ *  Copyright (C) 2024- Alberto Leiva Popper <ydahhrk@gmail.com>
+ *  Copyright (C) 2025- Antonio Quartulli <antonio@mandelbit.com>
+ */
+
+#ifndef MOD_XLAT_AUX_H_
+#define MOD_XLAT_AUX_H_
+
+/* Stuff that should maybe be added to linux/include/uapi/linux/icmp.h */
+
+#define icmp4_unused		un.gateway
+#define icmp4_datagram_length	un.reserved[1]
+
+/* Code 0 for ICMP type ICMP_PARAMETERPROB */
+#define ICMP_PTR_INDICATES_ERR	0
+/* Code 2 for ICMP type ICMP_PARAMETERPROB */
+#define ICMP_BAD_LENGTH		2
+
+#endif /* MOD_XLAT_AUX_H_ */
