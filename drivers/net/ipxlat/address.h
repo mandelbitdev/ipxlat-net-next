2722e87ea322555178abd4f290842c9bb378abbf
Ralf Lici <ralf@mandelbit.com>
2026-02-26 12:51:34 +0100

    ipxlat: add RFC6052 address conversion helpers
    Introduce IPv4/IPv6 stateless address mapping helpers used by the
    translation pipeline. Add the core 4<->6 conversion routines, including
    RFC6052 prefix embedding/extraction and the RFC6791 fallback source
    selection logic used by ICMP translation paths.
    
    Signed-off-by: Ralf Lici <ralf@mandelbit.com>

---
+++ drivers/net/ipxlat/address.h
@@ -0,0 +1,60 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*  IPXLAT - Stateless IP/ICMP Translation virtual device driver
+ *
+ *  Copyright (C) 2024- Alberto Leiva Popper <ydahhrk@gmail.com>
+ *  Copyright (C) 2026- Mandelbit, SRL
+ *
+ *  Author:	Alberto Leiva Popper <ydahhrk@gmail.com>
+ *		Antonio Quartulli <antonio@mandelbit.com>
+ *		Ralf Lici <ralf@mandelbit.com>
+ */
+
+#ifndef _NET_IPXLAT_ADDRESS_H_
+#define _NET_IPXLAT_ADDRESS_H_
+
+#include <linux/ip.h>
+#include <net/ipv6.h>
+
+#include "ipxlpriv.h"
+
+/**
+ * ipxl_46_convert_addr - translate one IPv4 address into RFC 6052 IPv6 form
+ * @pool6: configured RFC 6052 prefix
+ * @addr4: IPv4 address to convert
+ * @addr6: output IPv6 address
+ */
+void ipxl_46_convert_addr(const struct ipv6_prefix *pool6, __be32 addr4,
+			  struct in6_addr *addr6);
+
+/**
+ * ipxl_64_convert_addrs - translate outer IPv6 endpoints into IPv4 pair
+ * @cfg: translator configuration
+ * @hdr6: source IPv6 header
+ * @icmp_err: source packet is ICMPv6 error
+ * @src: output IPv4 source address
+ * @dst: output IPv4 destination address
+ *
+ * For ICMPv6 error packets whose source is outside @cfg->pool6, this helper
+ * returns @cfg->pool6791v4 as RFC 6791 fallback source.
+ *
+ * Return: 0 on success, negative errno on non-translatable addresses.
+ */
+int ipxl_64_convert_addrs(const struct ipxl_cfg *cfg,
+			  const struct ipv6hdr *hdr6, bool icmp_err,
+			  __be32 *src, __be32 *dst);
+
+/**
+ * ipxl_46_convert_addrs - translate outer IPv4 endpoints into IPv6 pair
+ * @cfg: translator configuration
+ * @iph4: source IPv4 header
+ * @iph6: output IPv6 header (only saddr/daddr are updated)
+ */
+static inline void ipxl_46_convert_addrs(const struct ipxl_cfg *cfg,
+					 const struct iphdr *iph4,
+					 struct ipv6hdr *iph6)
+{
+	ipxl_46_convert_addr(&cfg->pool6, iph4->saddr, &iph6->saddr);
+	ipxl_46_convert_addr(&cfg->pool6, iph4->daddr, &iph6->daddr);
+}
+
+#endif /* _NET_IPXLAT_ADDRESS_H_ */
